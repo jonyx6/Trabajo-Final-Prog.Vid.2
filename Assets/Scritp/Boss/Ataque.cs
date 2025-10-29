5b5f10e4259be1ab68f3c62171da62197d54dadf
jonathan puyol <jonathanalberto.puyol@estudiantes.unahur.edu.ar>
2025-10-29 19:49:21 -0300

    comienzo de interfaz de usuario

--- Assets/Scritp/Boss/Ataque.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Ataque : ScriptableObject
-{
-    public abstract IEnumerator AtacarI(GameObject objetivo, GameObject atacante);
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Trabajo-Final-Prog.Vid.2
-trabajo final para la materia de programación de videojuegos 2 

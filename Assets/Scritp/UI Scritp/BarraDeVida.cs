125582f667c440a3eb9f1838ba3f1cae6b2d5a34
Franco <francoiannone2@gmail.com>
2025-11-21 10:55:07 -0300

    barra de vida se expande con vida maxima

--- Assets/Scritp/UI Scritp/BarraDeVida.cs
+++ Assets/Scritp/UI Scritp/BarraDeVida.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 public class BarraDeVida : MonoBehaviour
 {
     [SerializeField]
-    private Image image;
-    private Atributos atributosDelJugador;
+    private Image rellenoDeBarraDeVida;
+    [SerializeField]
+    private float MultiplicadorDeAncho = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
-        ObtenerAtributosDeJugador();
-        RenderizarVida(atributosDelJugador.Vida,atributosDelJugador.VidaMaxima);
-        atributosDelJugador.OnVidaChange += RenderizarVida;
-    }
-    public void ObtenerAtributosDeJugador()
+        //obtenemos el componente atributos del jugador
+        Atributos atributosDelJugador = ObtenerAtributosDeJugador();
+
+        //dibujamos la vida que tiene al inicio
+        DibujarVida(atributosDelJugador.Vida,atributosDelJugador.VidaMaxima);
+
+        //este evento es invocado cuando cambia la vida
+        //nos suscribimos a el para saber cuando hay que dibujar la barra de vida
+        atributosDelJugador.OnVidaChange += DibujarVida;
+
+        //la barra de vida solo se dibuja cuando empieza y cuando cambia la vida no hace falta dibujarla en el update
+    } 
+    private Atributos ObtenerAtributosDeJugador()
     {
-        atributosDelJugador = GameObject.FindGameObjectWithTag("Player").GetComponent<Atributos>();
+        return GameObject.FindGameObjectWithTag("Player").GetComponent<Atributos>();
     }
 
-    private void RenderizarVida(float vida,float vidaMaxima)
+    private void DibujarVida(float vida,float vidaMaxima)
     {
-        image.fillAmount = vida /vidaMaxima;
+        CambiarAnchoDeBarraSegun(vidaMaxima);
+        CambiarRellenoDeBarraSegun(vida,vidaMaxima);
+    }
+    private void CambiarAnchoDeBarraSegun(float vidaMaxima)
+    {
+        float anchoDeBarra = vidaMaxima * MultiplicadorDeAncho;
+        transform.localScale = new Vector2(anchoDeBarra,transform.localScale.y);
+    }
+    private void CambiarRellenoDeBarraSegun(float vida,float vidaMaxima)
+    {
+        rellenoDeBarraDeVida.fillAmount = vida /vidaMaxima;
     }
 }

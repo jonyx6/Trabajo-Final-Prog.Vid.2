9071e4a7282996538ffde2a841ed94b3b8c3ff0d
Franco <francoiannone2@gmail.com>
2025-11-21 12:00:42 -0300

    items animados

--- Assets/Scritp/UI Scritp/Item.cs
+++ Assets/Scritp/UI Scritp/Item.cs
@@ -7,15 +7,31 @@ public class Item : MonoBehaviour
 {
     public TipoDeAtributo atributo;
     public float cantidadQueAumenta;
-/*     public int id;
-    public string type;
-    public string descripcion;
-    public Sprite icon;
-   
-    public bool estaAgarrado;
+    [SerializeField]
+    private float velMovimiento = 2;
+    [SerializeField]
+    private float amplitud = 0.3f;
+    private float posYInicial;
+    /*     public int id;
+        public string type;
+        public string descripcion;
+        public Sprite icon;
+
+        public bool estaAgarrado;
 
 
-    public bool estaEquipado; */
-
-
+        public bool estaEquipado; */
+    private void Start()
+    {
+        posYInicial = transform.position.y;
+    }
+    private void Update()
+    {
+        AnimarItem();
+    }
+    private void AnimarItem()
+    {
+        float posY = posYInicial + Mathf.Sin(Time.time * velMovimiento) * amplitud;
+        transform.position = new Vector2(transform.position.x, posY);
+    }
 }
